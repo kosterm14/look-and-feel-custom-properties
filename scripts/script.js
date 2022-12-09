460340626c03d4910d6266a429bdd0565f5bc7b3
Marco <marco.kosters@hva.nl>
2022-12-09 10:23:17 +0100

    javascript
    hele home page af  (charleys nieuwe design) + javascript interactie

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+function myFunction() {
+    var element = document.getElementById("myDIV");
+    element.classList.toggle("show");
+
+    var element = document.getElementById("myDIV2");
+    element.classList.toggle("show2");
+
+    var element = document.getElementById("myDIV3");
+    element.classList.toggle("show3");
+
+    var element = document.getElementById("myDIV4");
+    element.classList.toggle("show4");
+
+    var element = document.getElementById("text1");
+    element.classList.toggle("showText1");
+
+    var element = document.getElementById("text2");
+    element.classList.toggle("showText2");
+
+    var element = document.getElementById("text3");
+    element.classList.toggle("showText3");
+
+    var element = document.getElementById("text4");
+    element.classList.toggle("showText4");
+
+    var element = document.getElementById("text5");
+    element.classList.toggle("showText5");
+
+    var element = document.getElementById("button");
+    element.classList.toggle("button-reversed");
+  }
